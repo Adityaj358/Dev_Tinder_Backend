e934b7ccc95e9608e7c7930f6fa3a42dbe57070e
adityaj358 <adityajiwane182000@gmail.com>
2024-10-09 18:48:29 +0530

    learn handling the routes

--- src/app.js
+++ src/app.js
@@ -3,27 +3,15 @@ const express = require("express");
 const app = express();
 
 
-
-app.get("/user", (req,res)=> {
+app.get("/user/:userId/:name/:password", (req,res)=> {
+    console.log(req.params);
     res.send({
         firstname : "Aditya",
         lastname : "Jiwane"
     });
 })
 
-app.post("/user",(req,res) => {
-    console.log("save data to database");
-    res.send("Data saved succesfully to db");
-})
 
-
-app.use("/test" , (req,res) => {
-    res.send("Hello from the Test");
-});
-
-app.delete("/user", (req,res) => {
-    res.send("data successfully delete from db");
-})
 
 app.listen(3000, () => {
     console.log("Server is running successfully on port 3000....");
