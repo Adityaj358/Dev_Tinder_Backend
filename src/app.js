d614ebbdd97f49e6726d9f3ca530f5db33f7dca2
adityaj358 <adityajiwane182000@gmail.com>
2024-10-09 18:30:12 +0530

    exploring http requests

--- src/app.js
+++ src/app.js
@@ -3,15 +3,27 @@ const express = require("express");
 const app = express();
 
 
-app.use((req,res) => {
-    res.send("Hello Adi");
+
+app.get("/user", (req,res)=> {
+    res.send({
+        firstname : "Aditya",
+        lastname : "Jiwane"
+    });
+})
+
+app.post("/user",(req,res) => {
+    console.log("save data to database");
+    res.send("Data saved succesfully to db");
 })
 
 
 app.use("/test" , (req,res) => {
-    res.send("Hello Aditya");
+    res.send("Hello from the Test");
 });
 
+app.delete("/user", (req,res) => {
+    res.send("data successfully delete from db");
+})
 
 app.listen(3000, () => {
     console.log("Server is running successfully on port 3000....");
