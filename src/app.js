f89b3deefd4b5cc37fea216dfd2e4b7a746ed34d
adityaj358 <adityajiwane182000@gmail.com>
2024-11-22 18:08:10 +0530

    api level data sanitization

--- src/app.js
+++ src/app.js
@@ -16,6 +16,62 @@ app.post("/signup", async (req,res) =>{
   } 
 });
 
+// api to  get by email id
+app.get("/user",async (req,res) =>{
+  const userEmail = req.body.email
+
+  try {
+    const users = await User.findOne({email:userEmail})
+    if(!users){ 
+      res.status(404).send("User not found")
+    }else{
+      res.send(users)
+    }
+   
+  } catch (error) {
+    res.status(404).send("Something went wrong")
+  }
+})
+
+// app.delete("/user",async(req,res) => {
+//   const userId = req.body.User
+//   try {
+//     const user = await user.findByIdAndDelete(email)
+//   } catch (err) {
+//     res.status(400).send("Something went wrong")
+//   }
+// })
+
+app.patch("/user",async (req,res)=>{
+  const userId = req.body.userId
+  const data = req.body;
+
+  
+  try {
+
+    const ALLOWED_UPDATES = ["userID","photoUrl","about","gender","age","skills"];
+
+  const isAllowUpdates = Object.keys(data).every((k) => {
+    ALLOWED_UPDATES.includes(k)
+  })
+
+  if(!isAllowUpdates){
+    throw new Error("Update not allowed")
+  }
+
+  if(data.skills.length  > 10){
+    throw new Error("Skills cannot be more than 10")
+  }
+
+    const user = await User.findByIdAndUpdate({_id:userId},data,{
+      returnDocument: "after",
+      runValidator: true, 
+    });
+    res.send("User update Succesfully")
+  } catch (error) {
+    res.status(400).send("UPDATE FAILED"+ error.message)
+  }
+})
 
 connectDb().then( () => {
   console.log("Database Connection Established...");
--- src/models/user.js
+++ src/models/user.js
@@ -2,28 +2,57 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength:2,
+        maxLength:50,
     },
 
     lastName : {
-        type : String
+        type : String,
     },
 
     email : {
-        type : String
+        type : String,
+        required : true,
+        unique:true,
+        lowercase : true,
     },
 
     password : {
-        type : String
+        type : String,
+        required:true,
     },
 
     age : {
-        type : Number
+        type : Number,
+        min:18,
+        max:60,
     },
 
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male","female","other"].includes(value)){
+                throw new Error("Gender is not valid")
+            }
+        }
+    },
+
+    photoUrl : {
+        type: String
+    },
+    
+    about : {
+        type: String,
+        default: "This appliction for SDE",
+    },
+
+    skills:{
+        type:[String]
     }
+},{
+    timestamps:true
 });
 
 
